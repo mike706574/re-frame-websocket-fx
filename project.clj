a46fc89b8646a0127e19d6a90fe2d3d3fd83d0ff
EAS7510 <michaeleaster@northwesternmutual.com>
2018-01-31 17:59:30 -0600

    Initial.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject fun.mike/re-frame-websocket-fx "0.0.1-SNAPSHOT"
+  :description "A websocket effect handler for re-frame."
+  :url "http://github.com/mike706574/re-frame-spec-interceptor"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/spec.alpha "0.1.143"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :target-path "target/dev"
+                   :dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]])
---
+++ src/re_frame/websocket_fx.cljs
@@ -0,0 +1,17 @@
+(ns re-frame.websocket-fx
+  (:require [re-frame.core :as rf]))
+
+(defn websocket-effect
+  [{:as request
+    :keys [uri on-message on-error on-success on-failure existing-websocket]}]
+  (if existing-websocket
+    (rf/dispatch (conj on-success existing-websocket))
+    (let [websocket (js/WebSocket. uri)]
+      (set! (.-onmessage websocket) #(rf/dispatch (conj on-message %)))
+      (set! (.-onerror websocket) #(rf/dispatch (conj on-failure %)))
+      (set! (.-onopen websocket) (fn on-open []
+                                   (set! (.-onerror websocket) #(rf/dispatch (conj on-error %)))
+                                   (rf/dispatch (conj on-success websocket)))))))
+
+
+(rf/reg-fx :websocket websocket-effect)
