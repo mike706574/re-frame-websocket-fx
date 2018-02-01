03fd8173fd2f0f978b61358545bde27d6a5088e4
EAS7510 <michaeleaster@northwesternmutual.com>
2018-01-31 18:00:06 -0600

    Version 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/re-frame-websocket-fx "0.0.1"
+(defproject fun.mike/re-frame-websocket-fx "0.0.2-SNAPSHOT"
   :description "A websocket effect handler for re-frame."
   :url "http://github.com/mike706574/re-frame-spec-interceptor"
   :license {:name "Eclipse Public License"
